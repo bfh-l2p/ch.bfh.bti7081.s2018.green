ee46598283b8c086166466c2a63037b592cb23f2
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2018-05-21 11:12:42 +0200

    Tests are now more solid

--- src/test/java/ch/bfh/bti7081/s2018/green/models/managers/PersonManagerTest.java
+++ src/test/java/ch/bfh/bti7081/s2018/green/models/managers/PersonManagerTest.java
@@ -1,19 +1,20 @@
 package ch.bfh.bti7081.s2018.green.models.managers;
 
+import ch.bfh.bti7081.s2018.green.models.entities.Person;
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.List;
-import org.junit.Assert;
-import org.junit.Test;
-
-import ch.bfh.bti7081.s2018.green.models.entities.Person;
+import java.util.stream.IntStream;
 
 public class PersonManagerTest {
-	
-	@Test
-	public void personManagerTest() throws ClassNotFoundException, ParseException {
+
+    @Test
+    public void personManagerTest() throws ClassNotFoundException, ParseException {
         SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
         Date dob1 = new Date(format.parse("14.10.1991").getTime());
         Person testperson1 = new Person(
@@ -26,7 +27,7 @@ public class PersonManagerTest {
                 "martinscheck91@gmail.com",
                 "0798340599"
         );
-        
+
         Date dob2 = new Date(format.parse("15.10.1992").getTime());
         Person testperson2 = new Person(
                 "Tobias",
@@ -38,7 +39,7 @@ public class PersonManagerTest {
                 "martinscheck92@gmail.com",
                 "0798340598"
         );
-        
+
         Date dob3 = new Date(format.parse("15.10.1993").getTime());
         Person testperson3 = new Person(
                 "Lukas",
@@ -50,41 +51,43 @@ public class PersonManagerTest {
                 "martinscheck93@gmail.com",
                 "0798340597"
         );
-        
+
         PersonManager testmanager = new PersonManager();
-                
+
         //persist test person
         testmanager.add(testperson1);
-        
+
         //retrieval of persisted test person
         Person retrievedtestperson1 = testmanager.get(testperson1.getId());
-        
+
         //test if persisted and retrieved person is identical
         Assert.assertEquals(testperson1.getId(), retrievedtestperson1.getId());
-        
+
         //update test person
         testperson1.setFirstName("Jakob");
         testmanager.update(testperson1);
         retrievedtestperson1 = testmanager.get(testperson1.getId());
         Assert.assertEquals(testperson1.getFirstName(), retrievedtestperson1.getFirstName());
-        
+
         //remove person
         testmanager.remove(testperson1);
-        
+
         //persist all test persons
         List<Person> listoftestpersons = Arrays.asList(testperson2, testperson3);
         listoftestpersons.stream().forEach(tp -> testmanager.add(tp));
-        
+
         //retrieval of all test persons
         List<Person> listofretrievedtestpersons = testmanager.findAll();
-        
+
         //remove all test persons
         listoftestpersons.stream().forEach(tp -> testmanager.remove(tp));
-        
+
         //test if ids of persisted and retrieved persons are identical
         int[] arrayoftestpersonsids = listoftestpersons.stream().mapToInt(tp -> tp.getId()).toArray();
         int[] arrayofretrievedtestpersonsids = listofretrievedtestpersons.stream().mapToInt(tp -> tp.getId()).toArray();
-        Assert.assertArrayEquals(arrayoftestpersonsids, arrayofretrievedtestpersonsids);
-        
-	}
+
+        Arrays.stream(arrayoftestpersonsids).forEach(tp -> {
+            Assert.assertTrue(Arrays.stream(arrayofretrievedtestpersonsids).anyMatch(r -> r == tp));
+        });
+    }
 }
