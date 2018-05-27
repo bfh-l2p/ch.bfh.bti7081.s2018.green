546da91f627f011bc66d61e0c9146ece3ddf810e
tscheckow <martinscheck91@gmail.com>
2018-05-27 15:26:22 +0200

    JournalEntryManager draft

--- src/main/java/ch/bfh/bti7081/s2018/green/models/managers/JournalEntryManager.java
+++ src/main/java/ch/bfh/bti7081/s2018/green/models/managers/JournalEntryManager.java
@@ -1,23 +1,41 @@
 package ch.bfh.bti7081.s2018.green.models.managers;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.persistence.TypedQuery;
 
 import ch.bfh.bti7081.s2018.green.models.entities.JournalEntry;
+import ch.bfh.bti7081.s2018.green.models.entities.Patient;
+import ch.bfh.bti7081.s2018.green.models.entities.Staff;
 
 public class JournalEntryManager extends Manager<JournalEntry> {
     public JournalEntryManager() {
     	this.entityclass = JournalEntry.class;
 	}
     
-    public List<JournalEntry> findByPatient(Integer id) {
-        setNewEntityManager();
+    public List<JournalEntry> findBy(Staff item) {
+        TypedQuery<JournalEntry> query = manager.createQuery("SELECT j FROM JournalEntry j WHERE authorId = :authorId", JournalEntry.class);
+		query.setParameter("authorId", item.getId());
+		return findByQuery(query);
+    }
+    
+    public List<JournalEntry> findBy(Patient item) {
         TypedQuery<JournalEntry> query = manager.createQuery("SELECT j FROM JournalEntry j WHERE patientId = :patientId", JournalEntry.class);
-        query.setParameter("patientId", id);
+		query.setParameter("patientId", item.getId());
+		return findByQuery(query);
+    }
+    
+    public List<JournalEntry> findBy(LocalDateTime date) {
+        TypedQuery<JournalEntry> query = manager.createQuery("SELECT j FROM JournalEntry j WHERE created = :created", JournalEntry.class);
+        query.setParameter("created", date);
+        return findByQuery(query);
+    }
+    
+    private List<JournalEntry> findByQuery(TypedQuery<JournalEntry> query) {
+    	setNewEntityManager();
         List<JournalEntry> items = query.getResultList();
-
         manager.close();
-        return items;
+    	return items;
     }
 }
