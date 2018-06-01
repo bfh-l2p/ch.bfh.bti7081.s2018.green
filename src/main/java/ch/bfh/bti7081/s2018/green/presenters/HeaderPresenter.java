b14623bb67300ab2efbcbefe26315f5fe144b47e
DESKTOP-VQ8RH3L\Matthias Keller <matthias.keller@bfh.ch>
2018-06-01 21:22:44 +0200

    Minor change

--- src/main/java/ch/bfh/bti7081/s2018/green/presenters/HeaderPresenter.java
+++ src/main/java/ch/bfh/bti7081/s2018/green/presenters/HeaderPresenter.java
@@ -4,65 +4,67 @@ import java.util.List;
 
 import com.vaadin.server.Sizeable.Unit;
 import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.UI;
 
 import ch.bfh.bti7081.s2018.green.DataContainer;
+import ch.bfh.bti7081.s2018.green.NavigatorUI;
 import ch.bfh.bti7081.s2018.green.models.entities.Patient;
 import ch.bfh.bti7081.s2018.green.models.managers.PatientManager;
 import ch.bfh.bti7081.s2018.green.views.HeaderView;
 
 public class HeaderPresenter {
 
-    private HeaderView view;
+	private HeaderView view;
 	private DataContainer data;
 
-    public HeaderPresenter (HeaderView view) {
-        this.view = view;
-        this.data = DataContainer.getInstance();
-        
-        enteredView();       
-    }
+	public HeaderPresenter(HeaderView view) {
+		this.view = view;
+		this.data = DataContainer.getInstance();
 
-    private void enteredView(){
-    	
-    	// Assemble ComboBox to select a patient and...
-    	assembleCboxPatients();
-    	
-    	// ...set Change-Listener
-        view.getCboxPatients().addValueChangeListener(event -> {            	
-        	// Check if selected patient is previous patient, if not, set new patient
-        	if(data.getCurrentPatient() == null ||data.getCurrentPatient().getId() != event.getValue().getId()) {
-            	data.setCurrentPatient(event.getValue());            	
-        	}
-        });
-    }
-    
-    private void assembleCboxPatients(){
-    	
-    	// Get ComboBox from View
-    	ComboBox<Patient> cboxPatients = this.view.getCboxPatients();
-                
-        // Get all available patients from DB
-        List<Patient> pList = new PatientManager().findAll();
-        
-        cboxPatients.setCaption("Select Patient:");        
-        cboxPatients.setItems(pList);
-        
-        // Set property that will be displayed on combobox
-        cboxPatients.setItemCaptionGenerator(Patient::getFullName);
-        
-        // Set size of ComboBox
-        cboxPatients.setHeight(30, Unit.PIXELS);
-        cboxPatients.setWidth(200, Unit.PIXELS);
-        
-        // First patient in list will be set by default
-        // Prevent empty selection
-        if(!pList.isEmpty()) {
-        	cboxPatients.setValue(pList.get(0));
-        	cboxPatients.setEmptySelectionAllowed(false);            
-        }
-    }
-    
-    public void addUserName(String username) {
-        view.getLblLoggedOnUser().setValue(username);
-    }
+		enteredView();
+	}
+
+	private void enteredView() {
+
+		// Assemble ComboBox to select a patient and...
+		assembleCboxPatients();
+
+		// ...set Change-Listener
+		view.getCboxPatients().addValueChangeListener(event -> {
+			data.setCurrentPatient(event.getValue());
+
+			// TODO get current view and refresh it by triggering navigator
+			System.out.println(UI.getCurrent().getClass().getSimpleName());
+		});
+	}
+
+	private void assembleCboxPatients() {
+
+		// Get ComboBox from View
+		ComboBox<Patient> cboxPatients = this.view.getCboxPatients();
+
+		// Get all available patients from DB
+		List<Patient> pList = new PatientManager().findAll();
+
+		cboxPatients.setCaption("Select Patient:");
+		cboxPatients.setItems(pList);
+
+		// Set property that will be displayed on combobox
+		cboxPatients.setItemCaptionGenerator(Patient::getFullName);
+
+		// Set size of ComboBox
+		cboxPatients.setHeight(30, Unit.PIXELS);
+		cboxPatients.setWidth(200, Unit.PIXELS);
+
+		// First patient in list will be set by default
+		// Prevent empty selection
+		if (!pList.isEmpty()) {
+			cboxPatients.setValue(pList.get(0));
+			cboxPatients.setEmptySelectionAllowed(false);
+		}
+	}
+
+	public void addUserName(String username) {
+		view.getLblLoggedOnUser().setValue(username);
+	}
 }
