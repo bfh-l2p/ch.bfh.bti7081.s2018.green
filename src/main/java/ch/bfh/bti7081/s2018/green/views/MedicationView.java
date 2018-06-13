3aeaf2fe9d0d9fd8f4cc49802492f063124b54fb
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2018-06-13 11:00:08 +0200

    Prepare medication view for styling

--- src/main/java/ch/bfh/bti7081/s2018/green/views/MedicationView.java
+++ src/main/java/ch/bfh/bti7081/s2018/green/views/MedicationView.java
@@ -1,24 +1,23 @@
 package ch.bfh.bti7081.s2018.green.views;
 
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Locale;
-
+import ch.bfh.bti7081.s2018.green.models.entities.Medication;
+import ch.bfh.bti7081.s2018.green.presenters.MedicationPresenter;
 import com.vaadin.navigator.View;
 import com.vaadin.ui.CustomLayout;
 import com.vaadin.ui.Grid;
 
-import ch.bfh.bti7081.s2018.green.models.entities.Medication;
-import ch.bfh.bti7081.s2018.green.presenters.MedicationPresenter;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Locale;
 
 
 public class MedicationView extends CustomLayout implements View {
 
     public static final String NAME = "medication";
-    
+
     private NavigationButton btnAddMedication;
     private Grid<Medication> grdMedicamentGridView = new Grid<>();
-  
+
     public MedicationView() {
         this.setTemplateName("medicationApp");
         buildView();
@@ -32,7 +31,7 @@ public class MedicationView extends CustomLayout implements View {
 
     private void buildView() {
         this.btnAddMedication = new NavigationButton("New medication", this);
-        this.btnAddMedication.addClickListener((clickEvent) -> this.getUI().addWindow(new MedicationPrescriptionView(null)) );
+        this.btnAddMedication.addClickListener((clickEvent) -> this.getUI().addWindow(new MedicationPrescriptionView(null)));
 
         this.addComponent(this.btnAddMedication, "addMedicationTab");
         medGridBuilder();
@@ -46,26 +45,29 @@ public class MedicationView extends CustomLayout implements View {
         // Allow column hiding
         grdMedicamentGridView.getColumns().forEach(column -> column.setHidable(true));
 
+        // populate columns
         grdMedicamentGridView.addColumn(Medication::getName).setCaption("Medicament");
-        grdMedicamentGridView.addColumn(Medication::isActive).setCaption("Active");
         grdMedicamentGridView.addColumn(m -> m.getStartDate().format(formatter)).setCaption("Start");
         grdMedicamentGridView.addColumn(m -> m.getEndDate().format(formatter)).setCaption("Stop");
         grdMedicamentGridView.addColumn(Medication::getPeriode).setCaption("Frequency");
         grdMedicamentGridView.addColumn(Medication::getDose).setCaption("Dosis");
         grdMedicamentGridView.addColumn(m -> m.getPrescriber().getFullName()).setCaption("Prescriber");
+
         grdMedicamentGridView.setId("medicationAppGrid");
         grdMedicamentGridView.setWidth(100, Unit.PERCENTAGE);
+        grdMedicamentGridView.setStyleGenerator(medication -> medication.isActive() ? "medActive" : "medNotActice");
+
         this.addComponent(grdMedicamentGridView, "dataGrid");
     }
-    
+
     public void setGrdMedicamentGridViewItems(List<Medication> items) {
         this.grdMedicamentGridView.setItems(items);
     }
-    
+
     public Grid<Medication> getMedicamentGrid() {
         return grdMedicamentGridView;
     }
-    
+
     public NavigationButton getBtnAddMedication() {
         return btnAddMedication;
     }
